ea7c380e30b97c6e97a6ee198d4fee0da17eab5f
Damien Le Thiec <damien@dlet.fr>
2024-09-23 13:15:41 +0200

    Try to drop Metabase Postgres beofre restore

--- copy_msj_db_to_app.sh
+++ copy_msj_db_to_app.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 dbclient-fetcher psql
+dropdb $METABASE_POSTGRESQL_URL
+createdb $METABASE_POSTGRESQL_URL
 pg_dump --clean --if-exists --format c --dbname $SCALINGO_POSTGRESQL_URL --no-owner --no-privileges --no-comments --exclude-schema 'information_schema' --exclude-schema '^pg_*' --exclude-table="ahoy_events" --exclude-table="ahoy_visits" --exclude-table="versions" --file dump.pgsql
-pg_restore --clean --if-exists --cascade --no-owner --no-privileges --no-comments --dbname $METABASE_POSTGRESQL_URL dump.pgsql
+pg_restore --clean --if-exists --no-owner --no-privileges --no-comments --dbname $METABASE_POSTGRESQL_URL dump.pgsql
