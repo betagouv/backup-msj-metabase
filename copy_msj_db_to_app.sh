a2501e4f43f246d5dd54bc32e6b0dc4f12c62e94
Damien Le Thiec <damien@dlet.fr>
2024-09-23 18:56:42 +0200

    Try to drop table manually

--- copy_msj_db_to_app.sh
+++ copy_msj_db_to_app.sh
@@ -2,4 +2,4 @@
 
 dbclient-fetcher psql
 pg_dump --clean --if-exists --format c --dbname $SCALINGO_POSTGRESQL_URL --no-owner --no-privileges --no-comments --exclude-schema 'information_schema' --exclude-schema '^pg_*' --exclude-table="ahoy_events" --exclude-table="ahoy_visits" --exclude-table="versions" --file dump.pgsql
-pg_restore --clean --create --if-exists --no-owner --no-privileges --no-comments --dbname $METABASE_POSTGRESQL_URL dump.pgsql
+pg_restore --clean --if-exists --no-owner --no-privileges --no-comments --dbname $METABASE_POSTGRESQL_URL dump.pgsql
