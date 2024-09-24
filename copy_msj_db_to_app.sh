c7a75c5b0cbc49c26456188edd62b5cff04b1ab4
Damien Le Thiec <damien@dlet.fr>
2024-09-24 14:46:54 +0200

    Drop each table with cascade using a loop

--- copy_msj_db_to_app.sh
+++ copy_msj_db_to_app.sh
@@ -2,4 +2,17 @@
 
 dbclient-fetcher psql
 pg_dump --clean --if-exists --format c --dbname $SCALINGO_POSTGRESQL_URL --no-owner --no-privileges --no-comments --exclude-schema 'information_schema' --exclude-schema '^pg_*' --exclude-table="ahoy_events" --exclude-table="ahoy_visits" --exclude-table="versions" --file dump.pgsql
+
+# Drop all tables in the target database
+psql $METABASE_POSTGRESQL_URL -c "
+DO \$\$
+DECLARE
+    r RECORD;
+BEGIN
+    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP
+        EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+    END LOOP;
+END
+\$\$;"
+
 pg_restore --clean --if-exists --no-owner --no-privileges --no-comments --dbname $METABASE_POSTGRESQL_URL dump.pgsql
